3765b6967423853a8f06360225422603557c4672
PimehT <tareerepimeh@gmail.com>
2024-09-03 22:17:47 +0100

    fixed merge conflicts in modal.scss & task.scss

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
 import Banner from '../Banner/Banner';
 import Task from '../Task/Task';
-<<<<<<< HEAD
 import ResetPwdModal from '../Modal/ResetPwdModal';
-=======
->>>>>>> 7f255d94b522ff33590463a74df7c8e9e1f4921e
 import { AuthProvider } from '../Context/authContext';
 import { ModalProvider } from '../Context/modalContext';
 import PrivateRoute from '../Routes/PrivateRoute';
@@ -23,10 +20,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Banner />} />
                 <Route path="/tasks" element={<PrivateRoute><Task /></PrivateRoute>} />
-<<<<<<< HEAD
                 <Route path='/reset-password' element={<ResetPwdModal />} />
-=======
->>>>>>> 7f255d94b522ff33590463a74df7c8e9e1f4921e
               </Routes>
             </main>
           </div>
